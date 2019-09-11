3a1427c8692e3a46fb08081bcacce03aa0ad80b1
james <jtwebco@gmail.com>
2019-09-11 15:46:49 -0400

    changes loader to fade right into page

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
-const loader = document.querySelector('#loader')
-const app = document.querySelector('#app')
+const loader = document.querySelector("#loader")
+const app = document.querySelector("#app")
+const logo = document.querySelector("#logo")
 const about = document.getElementById("about")
+const ufo = document.querySelector(".loader-ufo__body")
 const projects = document.getElementById("projects")
 const social = document.getElementById("social")
 let { aboutInactive, projectsInactive, currentInactive, socialInactive } = false
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    loader.classList.add('fade--out')
-    app.classList.remove('app__no-scroll')
+    loader.classList.add("fade--out")
+    app.classList.remove("app__no-scroll")
+    logo.classList.remove("remove")
+    logo.classList.add("fade--in")
     setTimeout(() => {
-      loader.classList.add('remove')
+      loader.classList.add("remove")
+      logo.classList.remove("hide")
     }, 1000)
   }, 1000)
 })
 
 const isVisible = e => {
   let rect = e.getBoundingClientRect()
-  return (rect.bottom >= 300 && rect.top <= (window.innerHeight - 300))
+  return rect.bottom >= 300 && rect.top <= window.innerHeight - 300
 }
 
 const animateOnScroll = () => {
   !aboutInactive && isVisible(about) ? about.classList.add("fade--in") : null
-  !projectsInactive && isVisible(projects) ? projects.classList.add("fade--in") : null
+  !projectsInactive && isVisible(projects)
+    ? projects.classList.add("fade--in")
+    : null
   !socialInactive && isVisible(social) ? social.classList.add("fade--in") : null
 
-  if(aboutInactive && projectsInactive && employerInactive && socialInactive) {
+  if (aboutInactive && projectsInactive && employerInactive && socialInactive) {
     window.removeEventListener("scroll", animateOnScroll)
   }
 }
